30b69b1ac250fa67e11754898483170a7e15a113
Yiheng <development.henryquan@gmail.com>
2024-10-19 14:38:20 +1100

    how to import all as one module

---
+++ wowsinfo/usecase/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlinx.serialization)
+}
+
+kotlin {
+    js {
+        moduleName = "wowsinfo-usecase"
+        binaries.executable()
+        generateTypeScriptDefinitions()
+        nodejs()
+        useEsModules()
+    }
+    jvm {
+        withJava()
+        testRuns.named("test") {
+            executionTask.configure {
+                useJUnitPlatform()
+            }
+        }
+    }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {}
+        jsMain.dependencies {}
+    }
+}
